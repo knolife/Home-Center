53c63ec8dd33ad1b21d4b9063de2e51d05c77122
knolife <knolife@knolife.com>
2021-09-07 00:10:15 +0200

    push test

--- src/main/java/com/knolife/home/center/hc/HcApplication.java
+++ src/main/java/com/knolife/home/center/hc/HcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HcApplication.class, args); //
+		SpringApplication.run(HcApplication.class, args); // test
 	}
 
 }
