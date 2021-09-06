012c9a649917b74d112f3cb0be863f9f86c933c2
Sergore <no@hotmail.com>
2021-09-06 23:33:25 +0200

    first commit

---
+++ src/main/java/com/knolife/home/center/hc/HcApplication.java
@@ -0,0 +1,13 @@
+package com.knolife.home.center.hc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knolife/home/center/hc/HcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knolife.home.center.hc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
